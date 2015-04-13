68458008ec02e90e4aaad8cca4e531913d6cf8c6
JustinLennox <justinanthonylennox@gmail.com>
2015-04-13 03:47:39 -0400

    Initial Commit

---
+++ MacArthur/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  MacArthur
+//
+
+//  Copyright (c) 2015 Justin Lennox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
